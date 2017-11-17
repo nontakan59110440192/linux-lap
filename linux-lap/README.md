c89700bf407b01ad387a8ac8c1e5e69d29b6aec3
nontakan59110440192 <nontakan.th.59@ubu.ac.th>
2017-11-17 16:21:06 +0700

    add Nontakancli project

---
+++ linux-lap/README.md
@@ -0,0 +1,21 @@
+# BoxMinfo
+
+666
+
+
+# Installation
+
+If you don't use `pipsi`, you're missing out.
+Here are [installation instructions](https://github.com/mitsuhiko/pipsi#readme).
+
+Simply run:
+
+    $ pipsi install .
+
+
+# Usage
+
+To use it:
+
+    $ 666 --help
+
---
+++ linux-lap/setup.py
@@ -0,0 +1,48 @@
+"""
+666
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='666',
+    version='666',
+    url='https://github.com/nontakan59110440192/linux-lap',
+    license='BSD',
+    author='Nontakan Thaoto',
+    author_email='nontakan.th.59@ubu.ac.th',
+    description='666',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            '666 = 666.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
